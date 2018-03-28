ba12de4039404f3f566ec69119ae9f33a9b535c3
Robert LeCheminant <lecheminant@gmail.com>
2018-03-28 15:50:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromEndOfArray(array, element) {
-  sliceArray = array.slice(0, 1);
+  sliceArray = array.splice(2, 1);
   return (sliceArray);
 }
 
