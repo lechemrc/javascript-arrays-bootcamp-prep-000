feb3b10dd8127cea0dbcfaf3a25cd692c9ee7f1b
Robert LeCheminant <lecheminant@gmail.com>
2018-03-28 14:24:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,51 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var array = [1];
+var element = 'foo';
+ 
+function addElementToBeginningOfArray(array, element){
+ var addElement = [element];
+ addElement = addElement.concat(array);
+ return (addElement);
+}
+ 
+function destructivelyAddElementToBeginningOfArray(array, element){
+ array.unshift(element);
+ return (array);
+}
+
+function addElementToEndOfArray(array, element){
+  var addElementEnd = [element];
+  addElementEnd = array.concat(addElementEnd);
+  return (addElementEnd);
+}
+ 
+function destructivelyAddElementToEndOfArray(array, element){
+ array.push(element);
+ return (array);
+}
+
+function accessElementInArray(array, index){
+  console.log(array[2]);
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+  array.slice(-2);
+  return (array);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift();
+  array;
+  return (array);
+}
+
+function removeElementFromEndOfArray(array, element) {
+  var sliceArray = array.slice(0, 1);
+  return (sliceArray);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element){
+  array.pop();
+  return (array);
+}
+
